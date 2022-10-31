544f214ea468d7162aa6d861d439bfb1d6f4eb2d
fjrjdev <thedromemafia@gmail.com>
2022-10-31 18:10:07 -0300

    config: spetacular config

--- _project/urls.py
+++ _project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularRedocView.as_view(url_name='schema'))
 ]
