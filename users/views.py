c5a6f2eb7489a1d17bfa3af50b6e15d585bcdffa
fjrjdev <thedromemafia@gmail.com>
2022-11-01 12:50:15 -0300

    refactor

--- users/views.py
+++ users/views.py
@@ -1,14 +1,9 @@
-from django.contrib.auth import authenticate
-
 from rest_framework.generics import ListCreateAPIView
-from rest_framework.views import APIView, Request, Response, status
-from rest_framework.authtoken.models import Token
-from rest_framework.views import Response, status
-
-from addresses.models import Address
 
 from .models import User
-from .serializers import UserSerializer, UserPostSerializer, LoginSerializer
+from .serializers import UserPostSerializer
+
+from addresses.models import Address
 
 
 class UserView(ListCreateAPIView):
@@ -19,24 +14,3 @@ class UserView(ListCreateAPIView):
         address = self.request.data.pop("address")
         user = serializer.save()
         Address.objects.create(**address, user=user)
-
-
-class LoginView(APIView):
-    def post(self, request: Request) -> Response:
-        serializer = LoginSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        user = authenticate(**serializer.validated_data)
-
-        if user:
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response(
-                {
-                    "token": token.key,
-                }
-            )
-
-        return Response(
-            {"detail": "invalid username or password"},
-            status.HTTP_400_BAD_REQUEST,
-        )
