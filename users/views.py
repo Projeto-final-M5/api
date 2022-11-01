b031d489446891225d0bf66610bde3dfc6149205
fjrjdev <thedromemafia@gmail.com>
2022-11-01 14:20:42 -0300

    fix: create user

--- users/views.py
+++ users/views.py
@@ -1,14 +1,14 @@
 from rest_framework.generics import ListCreateAPIView
 
 from .models import User
-from .serializers import UserPostSerializer
+from .serializers import UserSerializer
 
 from addresses.models import Address
 
 
 class UserView(ListCreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserPostSerializer
+    serializer_class = UserSerializer
 
     def perform_create(self, serializer):
         address = self.request.data.pop("address")
