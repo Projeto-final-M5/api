98b699c1edc173bf334338eb13119fe5f2df66b2
tiblazy <hauntedapply@gmail.com>
2022-11-05 10:03:59 -0300

    feat: test book view (Get, Get by Id)

--- books/tests/test_views.py
+++ books/tests/test_views.py
@@ -2,9 +2,8 @@ from django.urls import reverse
 
 from rest_framework.test import APITestCase
 
-from extra_datas.models import Extra_Data
 from users.models import User
-from users.tests.mocks import mock_user, mock_diff, mock_user_login
+from users.tests.mocks import mock_user, mock_diff, mock_user_login, mock_user_diff_login
 
 from books.models import Book
 from .mocks import mock_book, mock_extra_data, mock_genders
@@ -14,6 +13,7 @@ class BookViewTest(APITestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user_data = mock_user
+        cls.user_diff_data = mock_diff
 
         cls.book_data = mock_book
         cls.extra_data_data = mock_extra_data
@@ -21,6 +21,9 @@ class BookViewTest(APITestCase):
 
         cls.user = User.objects.create_user(**cls.user_data)
         cls.user_login = mock_user_login
+
+        cls.user_diff = User.objects.create_user(**cls.user_diff_data)
+        cls.user_diff_login = mock_user_diff_login
 
         cls.base_url_user = reverse("users")
         cls.login = reverse("login")
@@ -88,4 +91,74 @@ class BookViewTest(APITestCase):
         data = {"detail": "Authentication credentials were not provided."}
 
         self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data, data)
+
+    def test_can_get_books(self):
+        response = self.client.get(self.base_url, format="json")
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_get_book_by_id(self):
+        user_token = self.client.post(self.login, self.user_login, format="json")
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + user_token.data["token"])
+
+        book = self.client.post(
+            self.base_url,
+            {
+                **self.book_data,
+                "user": self.user_data,
+                "extra_data": self.extra_data_data,
+                "genders": self.gender_data,
+            },
+            format="json",
+        )
+
+        response = self.client.get(f"/api/book/{book.data['id']}/", format="json")
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_cant_get_book_without_authentication(self):
+        response = self.client.get(
+            f"/api/book/1/",
+            format="json",
+        )
+
+        data = {"detail": "Authentication credentials were not provided."}
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data, data)
+
+    def test_cant_get_book_with_wrong_id(self):
+        owner_token = self.client.post(self.login, self.user_login, format="json")
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + owner_token.data["token"])
+
+        book = self.client.post(
+            self.base_url,
+            {
+                **self.book_data,
+                "user": self.user_data,
+                "extra_data": self.extra_data_data,
+                "genders": self.gender_data,
+            },
+            format="json",
+        )
+
+        user_token = self.client.post(self.login, self.user_diff_login, format="json")
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + user_token.data["token"])
+
+        response = self.client.get(f"/api/book/{book.data['id']}/", format="json")
+
+        data = {"detail": "You do not have permission to perform this action."}
+        
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.data, data)
+
+    def test_cant_get_book_with_wrong_id(self):
+        user_token = self.client.post(self.login, self.user_login, format="json")
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + user_token.data["token"])
+
+        response = self.client.get(f"/api/book/1/", format="json")
+
+        data = {"detail": "Not found."}
+
+        self.assertEqual(response.status_code, 404)
         self.assertEqual(response.data, data)
