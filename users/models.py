c0b4076bba201491e07ab2eaca1586fa87b9dff5
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2022-11-09 14:21:48 -0300

    feat: metodo de notas criado, para o post retornará 5 e apos avaliação será feito um calculo

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from feed_back.models import  FeedBack
 from borroweds.models import Borrowed
+from books.models import Book   
 import uuid
 
 from borroweds.models import Borrowed
@@ -34,14 +35,31 @@ class User(AbstractUser):
     ]
 
     def get_nota(self)-> str:
-        feeds = FeedBack.objects.all()
+        
         valores = []
         for e in FeedBack.objects.all():
             valores.append(e)
        
         notas=[]
+        
         for item in valores:
             nota = Borrowed.objects.get(feed_back = item)
-            print(item,"1")
-        print(valores)
-        return "to"
+            feed = FeedBack.objects.get(id = item.id)
+            book = Book.objects.get(id = feed.borrowed.book_id)
+            if feed.borrowed.user_id == self.id:  
+                if feed.stars_renter != None:
+                    notas.append(feed.stars_renter)
+
+            
+            if book.user_id == self.id:
+                if feed.stars_owner != None:
+                    notas.append(feed.stars_owner)
+        notafinal = 5
+        if  len(notas)> 0:
+            notafinal =0
+            for numero in notas:
+                notafinal += numero
+       
+            notafinal = notafinal/len(notas)
+        
+        return notafinal
